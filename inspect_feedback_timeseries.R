108b2e7cdadaf1b5476da1c44a161ea8bb56ba80
Paul A. Bloom <pab2163@columbia.edu>
2022-12-15 15:57:43 -0500

    Create inspect_feedback_timeseries.R

---
+++ inspect_feedback_timeseries.R
@@ -0,0 +1,15 @@
+library(tidyverse)
+
+
+
+feedback = read_csv('a_DMN_Feedback_2_Scale5_all_frames_feedback.csv', col_names = c('frame', 'roi_max', 'cen', 'dmn'))
+
+
+feedback_long = feedback %>%
+    pivot_longer(c('cen', 'dmn'))
+
+
+ggplot(feedback_long, aes(x = frame, y = value, color = name)) +
+    geom_line() +
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 25) 
